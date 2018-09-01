64c04a616cdba39e2deb5b5338c0b7a431c0e252
Masaaki Terai <soundbell@gmail.com>
2018-09-01 14:16:24 +0900

    fixed

--- sensor_humid.py
+++ sensor_humid.py
@@ -7,29 +7,29 @@ from retry import retry
 
 @retry()
 def func1(i2c,address):
-  i2c.write_i2c_block_data(address,0x00,[])
+    i2c.write_i2c_block_data(address,0x00,[])
 
 @retry()
 def func2(i2c,address):
-  i2c.write_i2c_block_data(address,0x03,[0x00,0x04])
+    i2c.write_i2c_block_data(address,0x03,[0x00,0x04])
 
 @retry()
 def func3(i2c,address):
-  ret = i2c.read_i2c_block_data(address,0,6)
-  return(ret)
+    ret = i2c.read_i2c_block_data(address,0,6)
+    return(ret)
    
 def get_value():
-  i2c = smbus.SMBus(1)
-  address = 0x5c
-  func1(i2c,address)
-  sleep(0.003)
-  func2(i2c,address)
-  sleep(0.015)
-  block = func3(i2c,address)
-  humidity = (block[2] << 8 | block[3])/10.0
-  temperature = (block[4] << 8 | block[5])/10.0
-  unix_time=int(time.time())
-  print("humidity\t%f\t%d" % (humidity,unix_time))
+    i2c = smbus.SMBus(1)
+    address = 0x5c
+    func1(i2c,address)
+    sleep(0.003)
+    func2(i2c,address)
+    sleep(0.015)
+    block = func3(i2c,address)
+    humidity = (block[2] << 8 | block[3])/10.0
+    temperature = (block[4] << 8 | block[5])/10.0
+    unix_time=int(time.time())
+    print("humidity\t%f\t%d" % (humidity,unix_time))
 
 if __name__ == '__main__':
     get_value()
--- upload_s3.py
+++ upload_s3.py
@@ -11,31 +11,39 @@ cam_name = "cam01"
 homedir = os.environ['HOME']
 print(homedir)
 
-if not os.path.exists("%s/cam_images/" % homedir):
-    print("error: cam_images dir does not exist.")
-    exit()
+def upload_images(s3):
+    if not os.path.exists("%s/cam_images/" % homedir):
+        print("error: cam_images dir does not exist.")
+        exit()
 
-fns = glob("%s/cam_images/*.jpg" % homedir)
-print(fns)
+    fns = glob("%s/cam_images/*.jpg" % homedir)
+    print(fns)
 
-#s3 = boto3.resource('s3')
-s3 = boto3.client('s3')
+    for fn in fns:
+        if os.path.isfile(fn):
+            print(fn)
+            #f = open(fn,"rb")
+            basename = os.path.basename(fn)
+            s3.upload_file(fn, bucket_name, "%s/%s" % (cam_name, basename))
+            #f.close()
+            os.remove(fn)
 
-for fn in fns:
-    if os.path.isfile(fn):
-        print(fn)
-        #f = open(fn,"rb")
-        basename = os.path.basename(fn)
-        s3.upload_file(fn, bucket_name, "%s/%s" % (cam_name, basename))
-        #f.close()
-        os.remove(fn)
+def upload_sensor(s3):
+    if not os.path.exists("%s/sensor/" % homedir):
+        print("error: sensor dir does not exist.")
+        exit()
 
-fns = glob("%s/sensor/*.log" % homedir)
-print(fns)
+    fns = glob("%s/sensor/*.log" % homedir)
+    print(fns)
 
-for fn in fns:
-    if os.path.isfile(fn):
-        print(fn)
-        basename = os.path.basename(fn)
-        s3.upload_file(fn, bucket_name, "%s/%s" % (cam_name, basename))
+    for fn in fns:
+        if os.path.isfile(fn):
+            print(fn)
+            basename = os.path.basename(fn)
+            s3.upload_file(fn, bucket_name, "%s/%s" % (cam_name, basename))
 
+if __name__ == '__main__':
+    #s3 = boto3.resource('s3')
+    s3 = boto3.client('s3')
+    upload_images(s3)
+    upload_sensor(s3)
