e8601c854f27ef39b1037e6610141df0f2f55ea7
Masaaki Terai <soundbell@gmail.com>
2018-09-01 22:02:49 +0900

    fixed

--- wrapper_sensor_humid.sh
+++ wrapper_sensor_humid.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-inst_dir=/home/pi/raspi_homesensor
+home_dir=/home/pi
 cam_name="raspcam01"
 mkdir -p ${inst_dir}/sensor
-${inst_dir}/raspi_homesensor/sensor_humid.py >> ${inst_dir}/sensor/${cam_name}_sensor_humid.log
+${home_dir}/raspi_homesensor/sensor_humid.py >> ${home_dir}/sensor/${cam_name}_sensor_humid.log
--- wrapper_sensor_temp.sh
+++ wrapper_sensor_temp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-inst_dir=/home/pi/raspi_homesensor
+home_dir=/home/pi/raspi_homesensor
 cam_name="raspcam01"
 mkdir -p ${inst_dir}/sensor
-${inst_dir}/raspi_homesensor/sensor_temp.py >> ${isnt_dir}/sensor/${cam_name}_sensor_temp.log
+${home_dir}/raspi_homesensor/sensor_temp.py >> ${home_dir}/sensor/${cam_name}_sensor_temp.log
