7b5c4406aac6a43d374e5eb649c6718d4cbf1c7d
Masaaki Terai <soundbell@gmail.com>
2018-09-01 13:38:40 +0900

    fixed

--- upload_s3.py
+++ upload_s3.py
@@ -5,11 +5,14 @@ import os.path
 import boto3
 from glob import glob
 
+bucket_name = "raspz"
+cam_name = "cam01"
+
 homedir = os.environ['HOME']
 print(homedir)
 
 if not os.path.exists("%s/cam_images/" % homedir):
-    print("error: cam_images does not exist.")
+    print("error: cam_images dir does not exist.")
     exit()
 
 fns = glob("%s/cam_images/*.jpg" % homedir)
@@ -23,7 +26,7 @@ for fn in fns:
         print(fn)
         #f = open(fn,"rb")
         basename = os.path.basename(fn)
-        s3.upload_file(fn, "raspiz", "cam01/%s" % basename)
+        s3.upload_file(fn, bucket_name, "%s/%s" % (cam_name, basename))
         #f.close()
         os.remove(fn)
 
@@ -34,5 +37,5 @@ for fn in fns:
     if os.path.isfile(fn):
         print(fn)
         basename = os.path.basename(fn)
-        s3.upload_file(fn, "raspiz", "room_env01/%s" % basename)
+        s3.upload_file(fn, bucket_name, "%s/%s" % (cam_name, basename))
 
--- wrapper.sh
+++ wrapper.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ./cam.sh
+./room_env_humid.sh
+./room_env_temp.sh
 ./upload_s3.py
