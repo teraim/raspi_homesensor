50a6cc4e4e5f0a826553eb787830be4b3d1d3b96
Masaaki Terai <soundbell@gmail.com>
2018-09-19 12:37:28 +0000

    fixed

--- sensor_HTU21DF.py
+++ sensor_HTU21DF.py
@@ -15,18 +15,14 @@ dt_now = datetime.now()
 dt_str1 = dt_now.strftime("%Y/%m/%d %H:%M:%S")
 dt_str2 = dt_now.strftime("%Y%m%d")
 
-fn1 = "%s/sensor/%s_sensor_temp_%s.csv" % (home_dir, cam_name, dt_str2)
-fn2 = "%s/sensor/%s_sensor_humid_%s.csv" % (home_dir, cam_name, dt_str2)
+fn = "%s/sensor/%s_sensor_%s.csv" % (home_dir, cam_name, dt_str2)
 
-f1 = open(fn1, "a")
-f2 = open(fn2, "a")
+f = open(fn, "a")
 
 HTU21DF.htu_reset()
 temp = HTU21DF.read_temperature()
 humid = HTU21DF.read_humidity()
 
-f1.write("%s,%s\n" % (dt_str1, temp))
-f2.write("%s,%s\n" % (dt_str1, humid))
+f.write("%s,%s,%s\n" % (dt_str1, temp, humid))
 
-f1.close()
-f2.close()
+f.close()
